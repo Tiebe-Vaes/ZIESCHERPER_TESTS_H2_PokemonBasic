910b95b1756e84cc67ce577f8d20e1028e8d8632
TiebeVS <tiebevaes@gmail.com>
2025-02-25 15:06:09 +0100

    25/2

--- PokemonGottaCatchmAll/Pokemon.cs
+++ PokemonGottaCatchmAll/Pokemon.cs
@@ -8,6 +8,60 @@ namespace PokemonGottaCatchmAll
 {
     public class Pokemon
     {
+        public int HP_Base { get; set; }
+        public int Attack_Base { get; set; }
+        public int Defense_Base { get; set; }
+        public int SpecialAttack_Base { get; set; }
+        public int SpecialDefense_Base { get; set; }
+        public int Speed_Base { get; set; }
 
+        public string Naam { get; set; }
+        public string Type { get; set; }
+        public int Nummer { get; set; }
+
+        public int Level { get; private set; }
+
+        public void VerhoogLevel()
+        {
+            Level++;
+        }
+
+        public double Average => (HP_Base + Attack_Base + Defense_Base + SpecialAttack_Base + SpecialDefense_Base + Speed_Base) / 6.0;
+        public int Total => HP_Base + Attack_Base + Defense_Base + SpecialAttack_Base + SpecialDefense_Base + Speed_Base;
+
+        public int HP_Full => (((HP_Base + 50) * Level) / 50) + 10;
+        public int Attack_Full => ((Attack_Base * Level) / 50) + 5;
+        public int Defense_Full => ((Defense_Base * Level) / 50) + 5;
+        public int SpecialAttack_Full => ((SpecialAttack_Base * Level) / 50) + 5;
+        public int SpecialDefense_Full => ((SpecialDefense_Base * Level) / 50) + 5;
+        public int Speed_Full => ((Speed_Base * Level) / 50) + 5;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Naam} (level {Level})  ");
+
+            Console.WriteLine("Base stats:");
+
+            Console.WriteLine($"     * Health: {HP_Base} ");
+            Console.WriteLine($"     * Attack: {Attack_Base} ");
+            Console.WriteLine($"     * Defense: {Defense_Base} ");
+            Console.WriteLine($"     * Special Attack: {SpecialAttack_Base} ");
+            Console.WriteLine($"     * Special Defense: {SpecialDefense_Base} ");
+            Console.WriteLine($"     * Speed: {Speed_Base} ");
+
+            Console.WriteLine("Full stats:");
+            
+            Console.WriteLine($"     * Health: {HP_Full} ");
+            Console.WriteLine($"     * Attack: {Attack_Full} ");
+            Console.WriteLine($"     * Defense: {Defense_Full} ");
+            Console.WriteLine($"     * Special Attack: {SpecialAttack_Full} ");
+            Console.WriteLine($"     * Special Defense: {SpecialDefense_Full} ");
+            Console.WriteLine($"     * Speed: {Speed_Full} ");
+
+            Console.WriteLine("Average stat: " + Average);
+            Console.WriteLine("Total stat: " + Total);
+
+            Console.WriteLine("----------------------------------------------------");
+        }
     }
 }
--- PokemonGottaCatchmAll/Program.cs
+++ PokemonGottaCatchmAll/Program.cs
@@ -6,8 +6,56 @@ namespace PokemonGottaCatchmAll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pokemon charmander = new Pokemon
+            {
+                Naam = "Charmander",
+                Nummer = 4,
+                Type = "Fire",
+                HP_Base = 39,
+                Attack_Base = 52,
+                Defense_Base = 43,
+                SpecialAttack_Base = 60,
+                SpecialDefense_Base = 50,
+                Speed_Base = 65
+            };
+
+            Pokemon charmeleon = new Pokemon
+            {
+                Naam = "Charmeleon",
+                Nummer = 5,
+                Type = "Fire",
+                HP_Base = 58,
+                Attack_Base = 64,
+                Defense_Base = 58,
+                SpecialAttack_Base = 80,
+                SpecialDefense_Base = 65,
+                Speed_Base = 80
+            };
+
+            Pokemon charizard = new Pokemon
+            {
+                Naam = "Charizard",
+                Nummer = 6,
+                Type = "Fire/Flying",
+                HP_Base = 78,
+                Attack_Base = 84,
+                Defense_Base = 78,
+                SpecialAttack_Base = 109,
+                SpecialDefense_Base = 85,
+                Speed_Base = 100
+            };
+
+            charizard.ShowInfo();
             
+            charizard.VerhoogLevel();
+            charizard.ShowInfo();
+            
+ 
+
+
+
         }
+
+        
     }
 }
